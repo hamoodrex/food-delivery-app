d7a53be98f519d0a5bf4b2e70b5d4e3f01e54530
hamoodrex <hamoodrex@gmail.com>
2022-03-19 15:03:01 +0200

    Initial upload. No design implemented

---
+++ food-delivery-app/Controllers/HttpController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace food_delivery_app.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HttpController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<HttpController> _logger;
+
+        public HttpController(ILogger<HttpController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/test")]
+        public string GetHello()
+        {
+            byte[] value;
+            bool exists = HttpContext.Session.TryGetValue("test", out value);
+            if (exists)
+            {
+                int old_int = (int)HttpContext.Session.GetInt32("test");
+                HttpContext.Session.SetInt32("test",  ++old_int);
+                return old_int.ToString();
+            }
+            else
+            {
+                HttpContext.Session.SetInt32("test", 0);
+                return "Hello World";
+            }
+        }
+    }
+}
